521d8aa217e481b80895aa311cc2a66a63cf87de
alohamora <adityarora97@gmail.com>
2018-11-09 11:30:25 +0530

    final test

--- test/test.js
+++ test/test.js
@@ -12,13 +12,9 @@ contract('Full Test', (accounts) => {
     })
     it('register players', async () => {
         try{
-            console.log("asdfa");
             await contractInstance.register_second_player({value: web3.toWei(0.000000000000000099) ,from: player2});
-            console.log("asdf");
-            var x= contractInstance.count_getter({from:player1});
-            console.log("asdfa");
-            assert.equal(x, 1, "Player didn't get registered.");
-            console.log("asdfa");
+            var x= await contractInstance.count_getter({from:player1});
+            assert.equal(x, 2, "Player didn't get registered.");
         }
         catch (e) {
             assert.fail("Player didn't get registered.");
