1d109371d29de4ba44c4515dd7a31d9838e513be
alohamora <adityarora97@gmail.com>
2018-11-09 10:29:42 +0530

    players register

--- contracts/TicTacToe.sol
+++ contracts/TicTacToe.sol
@@ -1,5 +1,22 @@
 pragma solidity ^0.4.24;
 
 contract TicTacToe{
+    address player1;
+    address player2;
+    bytes32[] board;
+    uint participating_amount;
+    uint playerCount = 0;
 
+    constructor () public payable{
+        require(msg.value > 0, "A participating bid is required");
+        player1 = msg.sender;
+        playerCount = 1;
+    }
+
+    function register_second_player() public payable{
+        require(playerCount == 1, "Player limit exceeded");
+        require(msg.value == participating_amount, "Participating bid required");
+        require(msg.sender != player1, "Same players not allowed");
+        playerCount = 2;
+    }
 }
